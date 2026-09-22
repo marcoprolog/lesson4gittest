185fc796e2d01b51d8544313d4a625bdac90e704
Mads B. Madsen <madsbeiermadsen@gmail.com>
2026-09-22 08:50:23 +0200

    bil

--- Assets/TestScript.cs
+++ Assets/TestScript.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
+    bool bil = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
